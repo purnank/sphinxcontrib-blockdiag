abe69b29c818f979fea970d2965861f9adc6a26c
Purnank G <me@purnank.in>
2025-08-29 11:55:45 +0800

    Remove dependency of pkg_resources

--- sphinxcontrib/blockdiag.py
+++ sphinxcontrib/blockdiag.py
@@ -14,7 +14,7 @@ import os
 import re
 import posixpath
 import traceback
-import pkg_resources
+from importlib import metadata
 from collections import namedtuple
 from docutils import nodes
 from sphinx import addnodes
@@ -312,7 +312,7 @@ def setup(app):
     app.connect("doctree-resolved", on_doctree_resolved)
 
     return {
-        'version': pkg_resources.require('blockdiag')[0].version,
+        'version': metadata.version('blockdiag'),
         'parallel_read_safe': True,
         'parallel_write_safe': True,
     }
